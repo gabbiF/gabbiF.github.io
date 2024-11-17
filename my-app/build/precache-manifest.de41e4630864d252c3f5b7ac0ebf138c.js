42db45b6d24ed6e1cd5bc330d99b77a735154119
gff29 <gff29@cornell.edu>
2024-11-17 18:32:32 -0500

    testing 3

---
+++ my-app/build/precache-manifest.de41e4630864d252c3f5b7ac0ebf138c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3c236a17ec3354683ee093948cffe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60523e62ca381de7be46",
+    "url": "/static/css/main.f748d157.chunk.css"
+  },
+  {
+    "revision": "944d6f2e91c7fa4b12d4",
+    "url": "/static/js/2.8be9095e.chunk.js"
+  },
+  {
+    "revision": "fac1f3ff5e8ebdfed3580bcec7c38cc3",
+    "url": "/static/js/2.8be9095e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82594277ffdbbbbe6c96",
+    "url": "/static/js/3.f00e9902.chunk.js"
+  },
+  {
+    "revision": "60523e62ca381de7be46",
+    "url": "/static/js/main.14ec116b.chunk.js"
+  },
+  {
+    "revision": "4d1b5a3c7c62867fc360",
+    "url": "/static/js/runtime-main.508cbcb0.js"
+  }
+]);
